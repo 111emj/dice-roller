034302c1e1d19c16040db927a1a9e9e94284bcff
111emj <111emj111@gmail.com>
2022-11-25 19:18:28 -0500

    setup blueprint

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
+// use regex::Regex;
+
 fn main() {
-    // let args:Vec<String> = std::env::args().collect();
+    let args:Vec<String> = std::env::args().collect();
+
+    for arg in &args[1..]{
+        println!("{}",arg);
+    }
 }
 
-fn roll_die(sides: u8) -> u8{
+fn _roll_die(sides: u16) -> u16{
     random_number::random!(1..sides)
+}
+
+fn _reduce_expression(expression: String) -> u16{
+    let exp = expression.clone();
+
+    // let parentheses_regex = Regex::new("(.*)").unwrap();
+    
+    exp.parse::<u16>().unwrap()
 }
