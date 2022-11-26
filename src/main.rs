467e4f0d0be083f6e4b6d8e7f0b78ecaae914df5
111emj <111emj111@gmail.com>
2022-11-25 19:24:53 -0500

    Finish Blueprint

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let args:Vec<String> = std::env::args().collect();
 
     for arg in &args[1..]{
-        println!("{}",arg);
+        println!("{}",reduce_expression(arg));
     }
 }
 
@@ -12,10 +12,10 @@ fn _roll_die(sides: u16) -> u16{
     random_number::random!(1..sides)
 }
 
-fn _reduce_expression(expression: String) -> u16{
+fn reduce_expression(expression: &String) -> u16{
     let exp = expression.clone();
 
     // let parentheses_regex = Regex::new("(.*)").unwrap();
     
-    exp.parse::<u16>().unwrap()
+    exp.parse::<u16>().expect("Failed to Reduce Expression")
 }
