2b44854061b8bba49b6f8463cc39d33dbbd426ef
111emj <111emj111@gmail.com>
2022-11-26 14:17:34 -0500

    Reduce simple dice rolls such as d6 or 7d12

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use regex::{
     Match,
 };
 use std::collections::LinkedList;
+use random_number::random;
 
 fn main() {
     let args:Vec<String> = std::env::args().collect();
@@ -12,14 +13,14 @@ fn main() {
     }
 }
 
-fn _roll_die(sides: u16) -> u16{
-    random_number::random!(1..sides)
+fn roll_dice(quantity: u16, quality: u16) -> LinkedList<u16>{
+    (0..quantity).map(|_|random!(1..quality)).collect()
 }
 
 fn reduce_expression(expression: &String) -> String{
     let mut exp = expression.clone();
 
-    regex_replace(
+    regex_replace( // reduce parentheses and recur inside of each set
         &mut exp,
         r"\([^\)]*[^\(]*\)",
         |x: &str|{
@@ -29,6 +30,20 @@ fn reduce_expression(expression: &String) -> String{
                     .expect("Failed to mutate match")
                     .to_string();
                 reduce_expression(&y)
+        }
+    );
+
+    regex_replace( // reduce simple rolls ex: 13d17
+        &mut exp,
+        r"\d*d\d*",
+        |x: &str|{
+            let (qt,ql) = x.split_once("d").unwrap();
+            roll_dice(
+                qt.parse::<u16>().unwrap_or(1),
+                ql.parse::<u16>().expect("unknown dice quality")
+            )
+                .iter()
+                .sum::<u16>().to_string()
         }
     );
     
